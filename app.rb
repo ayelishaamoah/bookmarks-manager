3bb6c610fa32f2ab66e4476880e74f68ad12eaab
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-23 15:51:18 +0100

    Added view bookmarks feature

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello world'
+    erb :index
+  end
+
+  get '/bookmarks' do
+    erb :bookmarks
   end
 
   run! if app_file == $0
